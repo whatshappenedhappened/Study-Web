f56897783cde26b5c45c72ab7ef2eeb786b71d3e
whatshappenedhappened <ionic023@gmail.com>
2024-02-26 10:26:30 +0900

    2024-02-26_01
    - add portfolio temp links

---
+++ app.js
@@ -0,0 +1,37 @@
+// document.body.children[1].children[0].href = "https://www.naver.com";
+
+// let anchorElement = document.getElementById('external-link');
+// anchorElement.href = 'https://www.google.com';
+
+// let anchorQuery = document.querySelector('a');
+// anchorQuery.href = 'https://www.naver.com';
+// anchorQuery.style.textDecoration = 'none';
+
+
+// let headerElement = document.body.children[0];
+// console.dir(headerElement);
+
+// console.log(headerElement.parentElement);
+// console.log(headerElement.nextElementSibling);
+
+// headerElement = document.getElementById('main-header');
+// console.log(headerElement);
+
+// let secondParagraph = document.querySelector('p:nth-of-type(2)');
+// console.log(secondParagraph);
+// secondParagraph.innerHTML = "Changed Paragraph!";
+// secondParagraph.innerHTML += "<br>I got you!";
+
+let newLink = document.createElement('a');
+newLink.href = "https://www.google.com"
+newLink.textContent = "New Link!";
+
+let firstParagraph = document.querySelector('p');
+firstParagraph.appendChild(newLink);
+
+let header = document.querySelector('h1');
+header.remove();
+
+firstParagraph.parentElement.append(firstParagraph);
+
+
