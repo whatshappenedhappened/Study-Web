f5053494eb681f930f6e74ced7146d7538df7948
whatshappenedhappened <ionic023@gmail.com>
2024-02-28 09:29:23 +0900

    2024-02-28_01
    - Tic Tac Toe game page is still in progress: player config is implemented

--- scripts/app.js
+++ scripts/app.js
@@ -1,6 +1,24 @@
+let editedPlayer = 0;
+
+// 플레이어 목록
+const players = [
+    {
+        name: '',
+        symbol: 'X'
+    },
+    {
+        name: '',
+        symbol: 'O'
+    }
+];
+
 // 모달 변수
 const modalControl = document.querySelector('#config-overlay');
 const modalBackground = document.querySelector('#backdrop');
+// 폼 변수
+const formElement = document.querySelector('form');
+// 모달 에러 변수
+const errorsOutput = document.querySelector('#config-errors');
 
 // 플레이어 모달 Edit 버튼 동작
 const editPlayer1BtnElement = document.querySelector('#edit-player-1-btn');
@@ -12,5 +30,8 @@ editPlayer2BtnElement.addEventListener('click', openPlayerConfig);
 // 플레이어 Edit 모달 제어
 const cancelEditButton = document.querySelector('#cancel-button');
 
-cancelEditButton.addEventListener('click', cancelPlayerConfig);
-modalBackground.addEventListener('click', cancelPlayerConfig);
+cancelEditButton.addEventListener('click', closePlayerConfig);
+modalBackground.addEventListener('click', closePlayerConfig);
+
+// 폼 제어
+formElement.addEventListener('submit', savePlayerConfig);
--- scripts/config.js
+++ scripts/config.js
@@ -1,11 +1,40 @@
 // 플레이어 섹션 Edit 버튼 함수
-function openPlayerConfig() {
+function openPlayerConfig(event) {
+    editedPlayer = +event.target.dataset.playerid;  // 형변환
     modalControl.style.display = 'block';
     modalBackground.style.display = 'block';
 }
 
 // 플레이어 이름 편집 모달 제어 함수
-function cancelPlayerConfig() {
+function closePlayerConfig() {
+    const nameInput = document.querySelector('#playername');
+
+    // 기존 입력 초기화
+    nameInput.value = '';
+    formElement.firstElementChild.classList.remove('error');
+    errorsOutput.textContent = '';
+
     modalControl.style.display = 'none';
     modalBackground.style.display = 'none';
+}
+
+// 폼 제출 함수
+function savePlayerConfig(event) {
+    event.preventDefault();
+    const formData = new FormData(event.target);
+    const enteredPlayerName = formData.get('playername').trim();    // trim()메서드로 공백 제거
+
+    // 간단한 유효성 검사
+    if (!enteredPlayerName) {
+        event.target.firstElementChild.classList.add('error');
+        errorsOutput.textContent = "Please enter a valid name!";
+        return;
+    }
+
+    // 플레이어 1과 2중에서 선택 후 업데이트
+    const updatedPlayerData = document.querySelector(`#player-${editedPlayer}-data`);
+    updatedPlayerData.children[1].textContent = enteredPlayerName;
+    // 승자 출력에 사용할 배열에 플레이어 이름 저장
+    players[editedPlayer - 1].name = enteredPlayerName;
+    console.log(players[editedPlayer - 1]);
 }
